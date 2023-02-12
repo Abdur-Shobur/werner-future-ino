ae0a93a28137c271b517e89ff0cc7486f0c786d8
abdur-shobur <abdurshobur.developer@gmail.com>
2023-02-12 13:12:41 +0600

    sec added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{html,js}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				blue_primary: '#275ca5',
+				blue_secondary: '#0e2a60',
+				blue_heading: '#0e2a60',
+				blue_light: '#123386',
+			},
+			backgroundImage: {
+				hero_bg: "url('./src/images/werner-roofing-logo-1a.png')",
+			},
+		},
 	},
 	plugins: [],
 };
