cbfb32316fb8be686e26d4c7b13db117d65fd386
abdur-shobur <abdurshobur.developer@gmail.com>
2023-02-13 18:30:22 +0600

    add all]"

--- src/script.js
+++ src/script.js
@@ -2,5 +2,4 @@ let toggle = document.getElementById('toggle_handler');
 let toggle_items = document.getElementById('toggle_menu_items');
 toggle.addEventListener('click', () => {
 	toggle_items.classList.toggle('hidden');
-	toggle_items.style.transitionDuration = '5s';
 });
